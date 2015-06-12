199cdc972723f4a904f4bfbb24a5935ed6bb3326
terut <terut.dev+github@gmail.com>
2015-06-12 10:53:34 +0900

    Initial Commit

---
+++ Wayfaring/Wayfaring.h
@@ -0,0 +1,19 @@
+//
+//  Wayfaring.h
+//  Wayfaring
+//
+//  Created by Terunori Togo on 2015/06/12.
+//  Copyright (c) 2015年 terut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wayfaring.
+FOUNDATION_EXPORT double WayfaringVersionNumber;
+
+//! Project version string for Wayfaring.
+FOUNDATION_EXPORT const unsigned char WayfaringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wayfaring/PublicHeader.h>
+
+
