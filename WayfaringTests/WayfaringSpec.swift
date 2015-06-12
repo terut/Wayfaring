57be8df8ed63173884e4b10a3defac27ad664782
terut <terut.dev+github@gmail.com>
2015-06-12 15:12:50 +0900

    Prepare testing

---
+++ WayfaringTests/WayfaringSpec.swift
@@ -0,0 +1,17 @@
+//
+//  WayfaringSpec.swift
+//  WayfaringSpec
+//
+//  Copyright (c) 2015年 terut. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class WayfaringSpec: QuickSpec {
+    override func spec() {
+        it("is 3") {
+            expect(1+2).to(equal(3))
+        }
+    }
+}
